1357d20bc84be7c0853ad73fef219127fdd3bca9
Enjae Antonio <Enjaeantonio@outlook.com>
2022-11-30 14:06:47 -0600

    doing js

---
+++ assets/script/Score.js
@@ -0,0 +1,31 @@
+'use strict';
+
+class Score {
+    #date;
+    #points;
+    #percentage;
+
+    constructor(date, points, percentage) {
+        this.#date = date;
+        this.#points = points;
+        this.#percentage = percentage;
+    }
+
+    get date() {
+        return this.#date;
+    }
+    
+    get points() {
+        return this.#points;
+    }
+    
+    get percentage() {
+        return this.#percentage;
+    }
+
+    getInfo(){
+        return `Date: ${this.date} Points: ${this.points} Percentage: ${this.percentage}`
+    }
+}
+
+export { Score }
--- assets/script/index.js
+++ assets/script/index.js
@@ -1,11 +1,16 @@
 'use strict';
 
 import { select, onEvent } from "./utils.js";
+import { Score } from "./Score.js";
 
 const output = select('.output p');
 const startBtn = select('.start-btn');
 const playAgainBtn = select('.play-again');
 const userInput = select('.user-input');
+const timer = select('.timer h3')
+const pointCount = select('.count h3')
+const playSong = new Audio('./assets/img/electro-pop-124340.mp3')
+const wrongSound = new Audio('./assets/img/negative_beeps-6008.mp3')
 
 const words = ['dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building', 'population',
 'weather', 'bottle', 'history', 'dream', 'character', 'money', 'absolute',
@@ -22,12 +27,57 @@ const words = ['dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building',
 'famous', 'league', 'memory', 'leather', 'planet', 'software', 'update', 'yellow',
 'keyboard', 'window'];
 
-var randomWord = Math.floor(Math.random()*words.length);
+var randomWord = words.sort(() => Math.random() - 0.5);
+console.log(randomWord)
+var nextWord = randomWord[0 + 1]
+console.log(nextWord)
+var timeLeft = 100;
+var points = 0;
 
-console.log(words[randomWord])
+const arr = []
 
-output.innerText = words[randomWord]
-
-addEventListener('keyup', function(){
+onEvent('click', startBtn, function(){
+    output.innerText = randomWord[0 + 1];
     
+    // Start Timer
+    let timeExpire = setInterval(function(){
+        timeLeft -= 1;
+        timer.innerText = `Time: ${timeLeft}`
+
+        if(timeLeft <= 0){
+            clearInterval(timeExpire)
+            output.innerText = `Time is up!`
+        }
+    }, 1000)
+
+    console.log('song playing')
+    // playSong.play() 
+
 })
+
+addEventListener('keydown', function(event){
+    let newDate = new Date();
+    let todaysDate = newDate.toDateString();
+
+
+    if(event.keyCode === 13){
+        event.preventDefault();
+    if(userInput.value == output.innerText){
+        
+    }
+
+    }
+    
+}
+);
+function createScore(){
+ 
+let percentage = (100 * newPoints) / words.length;
+let newPerc = percentage.toFixed(2)
+
+const newScore = new Score(todaysDate, newPoints, newPerc);
+console.log(newScore.getInfo())
+
+}
+
+createScore()    
