d95ce2cbce8511d90daba386634736ce5980ca75
Enjae Antonio <Enjaeantonio@outlook.com>
2022-11-30 16:04:09 -0600

    doing result page

--- assets/script/index.js
+++ assets/script/index.js
@@ -10,8 +10,10 @@ const userInput = select('.user-input');
 const timer = select('.timer h3')
 const pointCount = select('.count h3')
 const playSong = new Audio('./assets/img/electro-pop-124340.mp3')
-const wrongSound = new Audio('./assets/img/negative_beeps-6008.mp3')
-
+playSong.volume = 0.15;
+const correctSound = new Audio('./assets/img/ding-126626.mp3')
+const wrongSound = new Audio('./assets/img/wrong-47985.mp3')
+const resultPage = select('.win-result')
 const words = ['dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building', 'population',
 'weather', 'bottle', 'history', 'dream', 'character', 'money', 'absolute',
 'discipline', 'machine', 'accurate', 'connection', 'rainbow', 'bicycle',
@@ -27,57 +29,94 @@ const words = ['dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building',
 'famous', 'league', 'memory', 'leather', 'planet', 'software', 'update', 'yellow',
 'keyboard', 'window'];
 
-var randomWord = words.sort(() => Math.random() - 0.5);
-console.log(randomWord)
-var nextWord = randomWord[0 + 1]
-console.log(nextWord)
-var timeLeft = 100;
-var points = 0;
-
+let shuffleWords = words.sort(() => Math.random() - 0.5);
+let timeLeft = 10;
+let points = 0;
+let currentWord = '';
 const arr = []
 
-onEvent('click', startBtn, function(){
-    output.innerText = randomWord[0 + 1];
+function getRandomWord(arr){
+    for (let i = 0; i < words.length; i++) {
+        arr[i] = Math.floor(Math.random() * arr.length);
+        return arr[arr[i]]
+    }
+}
+
+function startTimer(){
+        // Start Timer
+        let timeExpire = setInterval(function(){
+            timeLeft -= 1;
+            timer.innerText = `Time: ${timeLeft}`
     
-    // Start Timer
-    let timeExpire = setInterval(function(){
-        timeLeft -= 1;
-        timer.innerText = `Time: ${timeLeft}`
+            if(timeLeft <= 0){
+                clearInterval(timeExpire)
+                getScore()
+                output.innerText = `Time is up!`
+                resultPage.style.visibility = 'visible'
+            }
+        }, 1000)
+}
 
-        if(timeLeft <= 0){
-            clearInterval(timeExpire)
-            output.innerText = `Time is up!`
-        }
-    }, 1000)
 
-    console.log('song playing')
-    // playSong.play() 
+onEvent('click', startBtn, function(){
 
+    startTimer();    
+    playSong.play();
+    let randomWord = getRandomWord(words)
+    output.innerText = randomWord
+
+    addEventListener('keydown', function(event){
+
+        if(event.keyCode == 13){
+            event.preventDefault();
+                if(output.innerText == userInput.value){
+                    correctSound.play()
+                    userInput.value = '';
+                    output.innerText = `${randomWord = getRandomWord(words)}`;
+                    points++
+                   
+                    pointCount.innerText = `Points: ${points}`
+
+                    let wordIndex = Math.floor(Math.random()*words.length)
+
+                    currentWord = randomWord;
+
+                    arr.push(currentWord)
+
+                    currentWord.slice(wordIndex, 1)
+                } else if (output.innerText !== userInput.value) {
+                    wrongSound.play()
+                } else {
+                    getScore()
+                }
+    }
+    });
 })
 
-addEventListener('keydown', function(event){
+function getScore(){
+
+    var newPoints = points;
+    console.log(points)
     let newDate = new Date();
     let todaysDate = newDate.toDateString();
 
+    let percentage = (100 * newPoints) / words.length;
+    let newPerc = percentage.toFixed(2)
 
-    if(event.keyCode === 13){
-        event.preventDefault();
-    if(userInput.value == output.innerText){
-        
-    }
+    const newScore = new Score(todaysDate, newPoints, newPerc);
 
-    }
+        resultPage.style.visibility = 'visible'
+
+        resultPage.innerHTML = `
+        <div class="result-btn">
+                <h2>Date: ${todaysDate}</h2>
+                <h2>Your Points: ${newPoints}</h2> 
+                <h2>Percentage: ${newPerc}</h2>
+                <button class="button" id="play-again-win"> Play Again? </button>
+        `
     
 }
-);
-function createScore(){
- 
-let percentage = (100 * newPoints) / words.length;
-let newPerc = percentage.toFixed(2)
 
-const newScore = new Score(todaysDate, newPoints, newPerc);
-console.log(newScore.getInfo())
+// onEvent('click', playAgainBtn, function(){
 
-}
-
-createScore()    
+// })
